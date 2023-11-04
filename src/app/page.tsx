736669af0bbc578ea3e46dbce66f03cacdae2020
shinshi-onigoori <cantoca0702@gmail.com>
2023-11-04 19:37:12 +0900

    HomePageにて全記事取得apiを叩く処理とその値表示のhooks設定。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,61 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return <div>Hello</div>;
-}
+import React, { useEffect, useState } from "react";
+import { PostType } from "@/types";
+import Link from "next/link";
+import styled from "styled-components";
+import MainHeader from "./component/mainHeader";
+import BlogContent from "./component/portfolio/BlogContent";
+import PostButton from "./component/postButton";
+
+const fetchAllBlogs = async () => {
+  const res = await fetch(`http://localhost:3000/api/blog`, {
+    cache: "no-store", //SSR:リクエストごとにサーバーに対してAPIを叩いてもらう。更新頻度が高いため。 SSGの場合はforce-cache
+  });
+
+  const data = await res.json();
+  return data.posts;
+};
+
+const Home: React.FC = () => {
+  const [posts, setPosts] = useState<PostType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchAllBlogs();
+      setPosts(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <MainHeader>
+        <PostButton />
+      </MainHeader>
+      <MainContainer>
+        <ContentBlock>
+          {posts.map((post: PostType) => (
+            <BlogContent key={post.id} post={post} />
+          ))}
+        </ContentBlock>
+      </MainContainer>
+    </React.Fragment>
+  );
+};
+
+const MainContainer = styled.div`
+  width: 100%;
+  display: flex;
+  padding-top: 20px;
+  justify-content: center;
+`;
+const ContentBlock = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Home;
