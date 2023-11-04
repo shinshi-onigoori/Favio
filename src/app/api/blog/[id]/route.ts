1f87ca1b922aff190df4fb6edac782a973304f29
shinshi-onigoori <cantoca0702@gmail.com>
2023-11-04 19:40:37 +0900

    idに紐づいた、BLOGの詳細用API、BLOGの記事編集API、BLOG削除要APIの追加

---
+++ src/app/api/blog/[id]/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from "next/server";
+import { main } from "../route";
+import { PrismaClient } from "@prisma/client";
+
+//インスタンス化
+const prisma = new PrismaClient();
+
+//BLOGの詳細用API
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    console.log(id);
+    await main();
+    const post = await prisma.post.findFirst({ where: { id } });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+//BLOGの記事編集API
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    const { title, description } = await req.json();
+
+    await main();
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+//BLOG削除要API
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    await main();
+    const post = await prisma.post.delete({
+      where: { id },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
