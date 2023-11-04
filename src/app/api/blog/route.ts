cbf5d9f8d677f1d8b0fc5ec7ec9223e872722ee7
shinshi-onigoori <cantoca0702@gmail.com>
2023-11-04 19:39:01 +0900

    formatによる整理

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,5 +1,5 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { connect } from "http2";
+import { PrismaClient } from "@prisma/client";
+import { NextPage } from "next";
 import { NextResponse } from "next/server";
 
 //インスタンス化
@@ -12,7 +12,7 @@ export async function main() {
     } catch (err) {
         return Error("DB接続に失敗しました")
     };
-}
+};
 
 //BLOGの全記事取得API
 export const GET = async (req: Request, res: NextResponse) => {
@@ -25,21 +25,18 @@ export const GET = async (req: Request, res: NextResponse) => {
     } finally {
         await prisma.$disconnect();
     }
-    
 };
 
 //BLOGの投稿用API
 export const POST = async (req: Request, res: NextResponse) => {
     try{
-const { tittle, description } = await req.json();
-        
+        const { title, description } = await req.json();
         await main();
-        const post = await prisma.post.create({ date: [ tittle, description ] });
-        return NextResponse.json({message: "Success", posts}, {status: 200});
+        const post = await prisma.post.create({ data: { title, description } });
+        return NextResponse.json({message: "Success", post}, {status: 200});
     } catch (err) {
-        return NextResponse.json({message: "Error", err}, {status: 500})
+        return NextResponse.json({message: "Error", err}, {status: 500});
     } finally {
         await prisma.$disconnect();
     }
-    
 };
