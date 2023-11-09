11ebe2f6f8f06bc3495126b5dc220f375b27f02d
shinshi-onigoori <cantoca0702@gmail.com>
2023-11-09 16:56:26 +0900

    BlogContentDetailPageの作成

---
+++ src/app/pages/blog/detail/[id]/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import MainHeader from "@/app/component/mainHeader";
+import BlogContentDetail from "@/app/component/portfolio/detail/BlogContentDetail";
+import PostButton from "@/app/component/postButton";
+import { PostType } from "@/types";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import toast from "react-hot-toast";
+
+const getBlogById = async (id: number) => {
+  const res = await fetch(`http://localhost:3000/api/blog/${id}`, {});
+
+  const data = await res.json();
+  console.log(data);
+
+  return data.post;
+};
+
+const BlogContentDetailPage = ({ params }: { params: { id: number } }) => {
+  const [post, setPost] = useState<PostType | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getBlogById(params.id);
+        setPost(data);
+      } catch (error) {
+        toast.error("エラーが発生しました", { id: "1" });
+      }
+    };
+
+    fetchData();
+  }, [params.id]);
+
+  return (
+    <BlogContentDetailContainer>
+      <MainHeader>
+        <PostButton></PostButton>
+      </MainHeader>
+      <ContentDetailBlock>
+        {post && <BlogContentDetail post={post} />}
+      </ContentDetailBlock>
+    </BlogContentDetailContainer>
+  );
+};
+
+const BlogContentDetailContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+const ContentDetailBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding-top: 30px;
+  background-color: #eeeaec;
+`;
+
+export default BlogContentDetailPage;
