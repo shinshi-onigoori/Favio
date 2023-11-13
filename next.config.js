2a3bcec6b79320f9bbf7c47c945456e3a8b9f549
shinshi-onigoori <cantoca0702@gmail.com>
2023-11-13 23:28:03 +0900

    cors導入

--- next.config.js
+++ next.config.js
@@ -1,5 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // 対象APIのパスパターン
+        // 今回は src/app/api/ 配下にAPIを作っているので下記のようにする
+        source: "/api/:path*",
+        headers: [
+          {
+            // CORSを許可するオリジン
+            key: "Access-Control-Allow-Origin",
+            // すべてのオリジンを許可するなら * (アスタリスク)
+            // ただセキュリティ的にはよろしくないので注意
+            value: "*",
+          },
+          {
+            // 許可するメソッド
+            key: "Access-Control-Allow-Methods",
+            value: "*",
+          },
+          {
+            // 許可するリクエストヘッダ
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ],
+      },
+    ];
+  },
   compiler: {
     styledComponents: true,
   },
